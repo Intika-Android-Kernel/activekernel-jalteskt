99a283015466d144acb7fbb540e55994b9d55a25
bestmjh47 <bestmjh47@naver.com>
2014-09-10 17:51:31 +0900

    Revert "revert this commit"
    This reverts commit 304dbc5a072a1d937f9eaed85cdc5129d8f3ec9e.

--- ramdisk/ramdisk-common/sbin/ext/otgfix.sh
+++ ramdisk/ramdisk-common/sbin/ext/otgfix.sh
@@ -1,7 +1,7 @@
 #!/sbin/busybox sh
 
 mount -o rw,remount rootfs
-chmod 777 /storage/usbdisk
-chown media_rw /storage/usbdisk
-chown .media_rw /storage/usbdisk
-chown media_rw .media_rw /storage/usbdisk
+chmod 777 /storage/UsbD*
+chown media_rw /storage/UsbD*
+chown .media_rw /storage/UsbD*
+chown media_rw .media_rw /storage/UsbD*
