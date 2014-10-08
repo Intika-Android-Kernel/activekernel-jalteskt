af0f7ef85c66ab0544e442b025b9521c906e411f
bestmjh47 <bestmjh47@naver.com>
2014-10-08 21:06:20 +0900

    build: fixup direct build script

--- buildboot.sh
+++ buildboot.sh
@@ -1,1 +1,8 @@
+cd ramdisk/ramdisk-common
+find . | cpio -o -H newc | gzip > ../boot.img-ramdisk.gz
+cd ../../
 mkbootimg --base 0x10000000 --pagesize 2048 --kernel zImage --ramdisk ramdisk/boot.img-ramdisk.gz -o ramdisk/boot.img
+cd ramdisk
+zip -r Multiboot-E300S-bestmjh47_kernel_STOCK.zip META-INF boot.img
+mv -v Multiboot-E300S-bestmjh47_kernel_STOCK.zip ../
+cd ../
