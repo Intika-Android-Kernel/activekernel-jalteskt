0230ee50048919e203ee319c762df6262db96f6e
bestmjh47 <bestmjh47@naver.com>
2014-10-09 14:55:35 +0900

    script: re-write geniunecheck

--- ramdisk/ramdisk-common/res/cm11/init.geniunecheck.sh
+++ ramdisk/ramdisk-common/res/cm11/init.geniunecheck.sh
@@ -1,3 +1,6 @@
 #!/sbin/busybox sh
 # bestmjh47's kernel feature.
-[ -e /system/bestmjh47 ] || reboot -p
+
+[ -e /system/bestmjh47 ] || reboot 
+mount -o rw,remount rootfs
+rm -rf init.geniunecheck.sh
