0ecc030543efa04e4aba8128db663da40b94d658
bestmjh47 <bestmjh47@naver.com>
2014-10-16 22:43:39 +0900

    script: rename for CM

--- buildboot.sh
+++ buildboot.sh
@@ -3,6 +3,6 @@ find . | cpio -o -H newc | gzip > ../boot.img-ramdisk.gz
 cd ../../
 mkbootimg --base 0x10000000 --pagesize 2048 --kernel zImage --ramdisk ramdisk/boot.img-ramdisk.gz -o ramdisk/boot.img
 cd ramdisk
-zip -r Multiboot-E300S-bestmjh47_kernel_STOCK.zip META-INF boot.img
-mv -v Multiboot-E300S-bestmjh47_kernel_STOCK.zip ../
+zip -r Multiboot-E300S-bestmjh47_kernel_CM.zip META-INF boot.img
+mv -v Multiboot-E300S-bestmjh47_kernel_CM.zip ../
 cd ../
