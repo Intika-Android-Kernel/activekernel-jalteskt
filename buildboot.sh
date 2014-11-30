510b9dcfd530be27fdaa3db58c72802bcc79f4a6
bestmjh47 <bestmjh47@naver.com>
2014-11-30 22:22:49 +0900

    Revert "ramdisk: switch to lzma"
    This reverts commit c6c718f1684f37a102eb46e59a0ae5e1c5a83659.

--- buildboot.sh
+++ buildboot.sh
@@ -1,7 +1,7 @@
 cd ramdisk/ramdisk-common
-find . | fakeroot cpio -H newc -o | lzma -e -9 > ../boot.img-ramdisk.cpio.lzma
+find . | cpio -o -H newc | gzip > ../boot.img-ramdisk.gz
 cd ../../
-mkbootimg --base 0x10000000 --pagesize 2048 --kernel zImage --ramdisk ramdisk/boot.img-ramdisk.cpio.lzma -o ramdisk/boot.img
+mkbootimg --base 0x10000000 --pagesize 2048 --kernel zImage --ramdisk ramdisk/boot.img-ramdisk.gz -o ramdisk/boot.img
 cd ramdisk
 zip -r Multiboot-E300S-bestmjh47_kernel_CM.zip META-INF boot.img
 mv -v Multiboot-E300S-bestmjh47_kernel_CM.zip ../
