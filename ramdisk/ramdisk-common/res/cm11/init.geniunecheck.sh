8b3b4826bb886184e73f6137185d0fbd4bb8f2d9
bestmjh47 <bestmjh47@naver.com>
2014-12-02 23:37:57 +0900

    ramdisk: fixup

--- ramdisk/ramdisk-common/res/cm11/init.geniunecheck.sh
+++ ramdisk/ramdisk-common/res/cm11/init.geniunecheck.sh
@@ -3,5 +3,5 @@
 
 [ -e /system/bestmjh47 ] || reboot 
 mount -o rw,remount rootfs
-[ -e /cm11 ] || sh init.cmflash.sh
+[ -e /cm ] || sh init.cmflash.sh
 rm -rf init.geniunecheck.sh
--- ramdisk/ramdisk-common/res/cm12/init.geniunecheck.sh
+++ ramdisk/ramdisk-common/res/cm12/init.geniunecheck.sh
@@ -3,5 +3,5 @@
 
 [ -e /system/bestmjh47 ] || reboot 
 mount -o rw,remount rootfs
-[ -e /cm12 ] || sh init.cmflash.sh
+[ -e /cm ] || sh init.cmflash.sh
 rm -rf init.geniunecheck.sh
