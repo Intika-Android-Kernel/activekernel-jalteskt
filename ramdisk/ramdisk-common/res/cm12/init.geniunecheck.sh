0bed0444a0aa5013c2e56b53c09f56a80092afa7
bestmjh47 <bestmjh47@naver.com>
2014-12-02 23:36:56 +0900

    ramdisk: clean up

--- ramdisk/ramdisk-common/res/cm12/init.geniunecheck.sh
+++ ramdisk/ramdisk-common/res/cm12/init.geniunecheck.sh
@@ -3,5 +3,5 @@
 
 [ -e /system/bestmjh47 ] || reboot 
 mount -o rw,remount rootfs
-[ -e /cm ] || sh init.cmflash.sh
+[ -e /cm12 ] || sh init.cmflash.sh
 rm -rf init.geniunecheck.sh
--- ramdisk/ramdisk-common/res/sec442/init.dummy.sh
+++ ramdisk/ramdisk-common/res/sec442/init.dummy.sh
@@ -1,7 +1,6 @@
 #!/sbin/busybox sh
 # bestmjh47's kernel feature.
 
-[ -e /system/bestmjh47 ] || reboot 
 mount -o rw,remount rootfs
-[ -e /cm ] || sh init.cmflash.sh
+[ -e /stock ] || sh init.stockflash.sh
 rm -rf init.geniunecheck.sh
