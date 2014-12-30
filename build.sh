a2fff5fb8185099d539dd37d46f9efa7613494fd
bestmjh47 <bestmjh47@naver.com>
2014-12-30 19:18:51 +0900

    build: edited to build both cm and stock-based kernels

--- build.sh
+++ build.sh
@@ -29,11 +29,13 @@ cd ramdisk/ramdisk-common
 find . | fakeroot cpio -H newc -o | lzma -e -9 > ../boot.img-ramdisk.cpio.lzma
 cd ../../
 mkbootimg --base 0x10000000 --pagesize 2048 --kernel zImage --ramdisk ramdisk/boot.img-ramdisk.cpio.lzma -o ramdisk/boot.img
+cp -f ramdisk/boot.img ramdisk/data/media/stock.img
+cp -f ramdisk/boot.img $HOME/kernel/cm11_*/ramdisk/data/media/stock.img
+cp -f ramdisk/boot.img $HOME/kernel/cm11_*/ramdisk/boot.img
 cd ramdisk
-zip -r Multiboot-E300S-bestmjh47_kernel_STOCK.zip META-INF boot.img
-mv -v Multiboot-E300S-bestmjh47_kernel_STOCK.zip ../
+zip -r Multiboot_kernel_e300_v4.7.zip META-INF data boot.img
+mv -v Multiboot_kernel_e300_v4.7.zip ../
 cd ../
-echo ""
 echo done! 
 
 rm -rf usr/initramfs_data.cpio
