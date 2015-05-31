2e22e310867d4b8ada2b354aa1c89edf213210c4
bestmjh47 <bestmjh47@naver.com>
2015-05-31 15:24:24 +0900

    ramdisk: cm12.1: update to latest build

--- buildboot.sh
+++ buildboot.sh
@@ -3,9 +3,7 @@ find . | fakeroot cpio -H newc -o | lzma -e -9 > ../boot.img-ramdisk.cpio.lzma
 cd ../../
 mkbootimg --base 0x10000000 --pagesize 2048 --kernel zImage --ramdisk ramdisk/boot.img-ramdisk.cpio.lzma -o ramdisk/boot.img
 cp -f ramdisk/boot.img ramdisk/data/media/stock.img
-cp -f ramdisk/boot.img $HOME/kernel/cm11_*/ramdisk/data/media/stock.img
-cp -f ramdisk/boot.img $HOME/kernel/cm11_*/ramdisk/boot.img
 cd ramdisk
-zip -r Multiboot_kernel_e300_v5.0.zip META-INF data boot.img
-mv -v Multiboot_kernel_e300_v5.0.zip ../
+zip -r Activekernel_e300_v1.1.zip META-INF data boot.img
+mv -v Activekernel_e300_v1.1.zip ../
 cd ../
