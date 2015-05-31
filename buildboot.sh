0ce42dd7341a19cabe0a4a659494d24511eeec4e
bestmjh47 <bestmjh47@naver.com>
2015-05-31 15:25:41 +0900

    build: fix file name

--- buildboot.sh
+++ buildboot.sh
@@ -4,6 +4,6 @@ cd ../../
 mkbootimg --base 0x10000000 --pagesize 2048 --kernel zImage --ramdisk ramdisk/boot.img-ramdisk.cpio.lzma -o ramdisk/boot.img
 cp -f ramdisk/boot.img ramdisk/data/media/stock.img
 cd ramdisk
-zip -r Activekernel_e300_v1.1.zip META-INF data boot.img
-mv -v Activekernel_e300_v1.1.zip ../
+zip -r Multiboot_kernel_e300_v5.0.zip META-INF data boot.img
+mv -v Multiboot_kernel_e300_v5.0.zip ../
 cd ../
