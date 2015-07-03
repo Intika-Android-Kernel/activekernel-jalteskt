37fb62132189c4f5ffaf6ae2b5f1b81a9a47cee6
bestmjh47 <bestmjh47@naver.com>
2015-07-03 23:43:19 +0900

    build: Seperate kitkat and lollipop kernel

--- build.sh
+++ build.sh
@@ -30,8 +30,9 @@ find . | fakeroot cpio -H newc -o | lzma -e -9 > ../boot.img-ramdisk.cpio.lzma
 cd ../../
 mkbootimg --base 0x10000000 --pagesize 2048 --kernel zImage --ramdisk ramdisk/boot.img-ramdisk.cpio.lzma -o ramdisk/boot.img
 cp -f ramdisk/boot.img ramdisk/data/media/stock.img
-cp -f ramdisk/boot.img $HOME/kernel/cm11_*/ramdisk/data/media/stock.img
-cp -f ramdisk/boot.img $HOME/kernel/cm11_*/ramdisk/boot.img
+cp -f ramdisk/boot.img $HOME/kernel/cm11_*/ramdisk/data/media/kitkat.img
+#cp -f ramdisk/boot.img $HOME/kernel/cm11_*/ramdisk/boot.img
+cp -f ramdisk/boot.img $HOME/kernel/activekernel-lollipop/ramdisk/data/media/kitkat.img
 cd ramdisk
 zip -r Multiboot_kernel_e300_v5.0.zip META-INF data boot.img
 mv -v Multiboot_kernel_e300_v5.0.zip ../
